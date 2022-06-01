0c88efb4e9b45f1674cb4954b1a12d389c2b9773
Medger Deana <medeana@mi-w6.msk.21-school.ru>
2022-06-01 13:06:21 +0300

    gnl

---
+++ get_next_line.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: medeana <medeana@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/16 12:57:27 by medeana           #+#    #+#             */
+/*   Updated: 2021/10/23 12:19:01 by medeana          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+void			ft_strcpy(char *dest, char *src);
+size_t			ft_strlen(char *str);
+char			*ft_strchr(char *s, int c);
+char			*ft_strjoin(char *s1, char *s2);
+char			*get_next_line(int fd);
+char			*ft_remainder(char *remainder, char *line);
+char			*read_file(int fd, char *remainder);
+
+#endif
